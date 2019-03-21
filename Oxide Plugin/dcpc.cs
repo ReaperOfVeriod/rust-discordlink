0b0194808dc4babafca9b2f871dc0ef9a557be05
Peter Kuijpers <oeps46@gmail.com>
2019-03-21 21:13:32 +0100

    added start of plugin

---
+++ Oxide Plugin/dcpc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Oxide.Plugins
+{
+    [Info("dcpc", "ReaperOfVeriod", "0.1.0")]
+    [Description("Makes epic stuff happen")]
+    class EpicStuff : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("initialise dcpc");
+        }
+
+        [Command("test")]
+        private void TestCommand(IPlayer player, string command, string[] args)
+        {
+            player.Reply("Test successful!");
+        }
+
+    }
+}
