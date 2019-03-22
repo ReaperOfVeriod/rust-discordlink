bdcbbd59145c8190b59802db687ac3bbbf105b84
Peter Kuijpers <oeps46@gmail.com>
2019-03-22 10:57:06 +0100

    fix random crash

--- Client/main.js
+++ Client/main.js
@@ -8,6 +8,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('error', console.error);
+
 client.on('message', msg => {
   if (msg.content === "!code") {
 
