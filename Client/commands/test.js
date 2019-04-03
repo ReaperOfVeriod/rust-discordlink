307302a40a9258c88258723ebb2f55919de631ef
Peter Kuijpers <oeps46@gmail.com>
2019-04-03 20:25:10 +0200

    fixed test command

--- Client/commands/test.js
+++ Client/commands/test.js
@@ -2,52 +2,60 @@ const request = require('request');
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-    request('https://rust-servers.net/api/?object=servers&element=detail&key=fMfruWLJibpYrcIpGfg1zQ2koShJ5hdfVOH', { json: true }, (err, res, body) => {
-        if (err) {
-            return console.log(err); 
-        }
-        //console.log(body);
+	request('https://rust-servers.net/api/?object=servers&element=detail&key=fMfruWLJibpYrcIpGfg1zQ2koShJ5hdfVOH', { json: true }, (err, res, body) => {
+		if (err) {
+			return console.log(err);
+		}
+		//console.log(body);
 
-        let id = body.id;
-        let serverName = body.name;
-        let ipAdress = `Client.connect` + " " + body.address + `:` + body.port;
-        let location = body.location;
-        let mapType = body.map;
-        let playerCount = `${body.players}/${body.maxplayers}`;
-        let gameVersion = body.version;
-        let rank = body.rank;
-        let votes = body.votes;
-        let voteLink = `${body.url}vote/`;
+		let id = body.id;
+		let serverName = body.name;
+		let ipAdress = body.address + `:` + body.port;
+		let location = body.location;
+		let mapType = body.map;
+		let playerCount = `${body.players}/${body.maxplayers}`;
+		let gameVersion = body.version;
+		let rank = body.rank;
+		let votes = body.votes;
+		let voteLink = `${body.url}vote/`;
+		let uptime = `${body.uptime}%`;
+		let status = body.is_online;
 
-        const embed = new Discord.RichEmbed() //embed
-        .setTitle('Rusticaland Stats.')
-        .setThumbnail('https://images-ext-2.discordapp.net/external/Zs3Tr3G-0Z03XlKZD5rTkF9NIE0_aGsfDotdriSk1LA/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/550753683433062420/d5089bab0829653868e7b1b5b1354e3f.webp')
-        .setDescription(`
-        ** id **
-        ${id}
-        ** Server Name **
-        ${serverName}
-        ** IP **
-        ${ipAdress}
-        ** Location **
-        ${location}
-        ** Map Type **
-        ${mapType}
-        ** Player count **
-        ${playerCount}
-        ** Game Version **
-        ${gameVersion}
-        ** rank **
-        ${rank}
-        ** Votes **
-        ${votes}
-        ** Vote Link **
-        ${voteLink}
 
-        
-        `)
-        .setImage("https://cdn.discordapp.com/attachments/557640637151707136/561952828789227520/Banner.gif")
-        .setColor('#ff0000')
-    message.channel.send(embed)
-    });
+		if (body.is_online === 0) {
+			let status = `Offline`
+		} else {
+			let status = `Online`
+		}
+
+		request('https://rust-servers.net/api/?object=servers&element=voters&key=fMfruWLJibpYrcIpGfg1zQ2koShJ5hdfVOH&month=current&format=json&limit=1', { json: true }, (err, res, body1) => {
+			if (err) {
+				return console.log(err);
+			}
+			let topVoter = body1.voters[0].nickname;
+
+			const embed = new Discord.RichEmbed() //embed
+				.setColor('#32CD32')
+				.setTitle("Rusticaland's Status")
+				.setURL('https://rusticaland.net/')
+				.setThumbnail('https://i.imgur.com/ZbiCgRb.png')
+				.setDescription('')
+				//.addField('**ID**', id, true)
+				//.addField('**Status**', status, true)
+				.addField('**Server Name**', serverName, true)
+				.addField('**IP:PORT**', ipAdress, true)
+				.addField('**Location**', location, true)
+				.addField('**Map Type**', mapType, true)
+				.addField('**Player count**', playerCount, true)
+				.addField('**Game Version**', gameVersion, true)
+				.addField('**Rank **', rank, true)
+				.addField('**Votes**', votes, true)
+				.addField('**Vote Link**', 'Click [here](https://rust-servers.net/server/136181/vote/) to vote!', true)
+				.addField('**Top Voter**', topVoter, true)
+				.addField('**Uptime**', uptime, true)
+				.setTimestamp()
+				.setImage("https://i.imgur.com/XkC62H9.gif")
+			message.channel.send(embed)
+		});
+	})
 }
