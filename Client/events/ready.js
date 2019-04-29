c0c434346c78535dbcd1366d2a2898b8e3c49cca
Peter Kuijpers <oeps46@gmail.com>
2019-04-29 22:16:21 +0200

    Update ready.js

--- Client/events/ready.js
+++ Client/events/ready.js
@@ -7,12 +7,12 @@ module.exports = (client, ready) => {
   var interval = setInterval(function () {
     request('https://rust-servers.net/api/?object=servers&element=detail&key=fMfruWLJibpYrcIpGfg1zQ2koShJ5hdfVOH', { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
-      if (body.is_online === 0 ) {
-        let status = `server offline :(`
-        client.user.setActivity(status);
-      } else {
+      if (body.is_online !== 1 ) {
         let playing = `${body.players}/${body.maxplayers} online!`
         client.user.setActivity(playing);
+      } else {
+        let status = `server offline :(`
+        client.user.setActivity(status);
       }
     });   
   }, 1 * 50000);
